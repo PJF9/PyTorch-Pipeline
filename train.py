65806cc0ec7c486cb2102aaf233a7c3f892552db
PJF9 <jacobfloratos@gmail.com>
2024-06-25 20:19:10 +0300

    Add data utils to train.py to make the code more readable

--- train.py
+++ train.py
@@ -1,14 +1,17 @@
-from src.dataset import YourDataset
 from src.models import TransformerEncoderAll
 from src.training import Trainer, EarlyStopping
-from src.utils import plot_losses, get_accuracy, configure_logger, get_device
+from src.utils import (
+    plot_losses,
+    get_accuracy,
+    configure_logger,
+    get_device,
+    get_datasets
+)
 
 from torch import nn, optim
 from torch.optim.lr_scheduler import ExponentialLR
-from torch.utils.data import Dataset
 
 import os
-from typing import List
 
 
 # Get the logger for this module
@@ -18,10 +21,10 @@ logger = configure_logger(__name__)
 config = dict(
     DEVICE = get_device(),
 
-    DATASETS_PATH = '',
-    LOGS_PATH = '',
-    PLOTS_PATH = '',
-    MODELS_PATH = '',
+    DATASETS_PATH = '...',
+    LOGS_PATH = '...',
+    PLOTS_PATH = '...',
+    MODELS_PATH = '...',
     
     WINDOW = 10,
     BATCH_SIZE = 16,
@@ -45,40 +48,11 @@ model_kwards = dict(
 )
 
 
-def get_datasets(n_test: int=1) -> List[Dataset]:
-    '''
-    Prepare training and validation datasets.
-
-    Args:
-        n_test (int, optional): Number of datasets to use for testing. Defaults to 1.
-
-    Returns:
-        List[Dataset]: The List of datasets.
-    '''
-    datasets = []
-
-    for dataset_path in os.listdir(config.DATASETS_PATH)[n_test:]: # Keeping the first 2 datasets as the testing sets
-        # Load the dataset
-        '''
-        The code for creating a dataset instance goes here, etc:
-            df = pd.read_csv(os.path.join(config.DATASETS_PATH, dataset_path))
-            dataset = YourDataset(df, config.WINDOW, normilize=True, to_tensors=True)
-        '''
-        dataset = YourDataset(...)
-        datasets.append(dataset)
-
-        logger.info(f'Dataset `{dataset_path}` has been loaded (with {len(dataset)} samples).')
-
-    logger.info(f'Datasets are ready ({len(datasets)} have been loaded)')
-
-    return datasets
-
-
 def main() -> None:
     '''Main function to train the deep learning model.'''
 
     ## Create the training and validation data loaders
-    datasets = get_datasets(n_test=2)
+    datasets = get_datasets(config.DATASETS_PATH, n_test=2)
 
     # Instanciate the Model
     model = TransformerEncoderAll(window=config.WINDOW, **model_kwards).to(config.DEVICE, non_blocking=True)
@@ -90,7 +64,6 @@ def main() -> None:
 
     opt = optim.AdamW(model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEIGHT_DECAY)
     scheduler = ExponentialLR(opt, gamma=config.GAMMA)
-
     stopper = EarlyStopping(patience=config.PATIENCE, threshold=config.STOP_THRESH)
 
     # Instanciate the Trainer
@@ -115,6 +88,8 @@ def main() -> None:
         save_per = config.EPOCHS,
         save_path = config.MODELS_PATH
     )
+
+    logger.info(f'Training Results: {train_res}')
 
     # Plot the losses and save them
     plot_losses(train_res['train_loss'], train_res['valid_loss'], save_path=os.path.join(config.PLOTS_PATH, 'losses.png'))
