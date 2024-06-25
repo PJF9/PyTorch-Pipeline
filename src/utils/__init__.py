693b09d03274de2174a5bb2836b4084b2c67a339
PJF9 <jacobfloratos@gmail.com>
2024-06-25 20:16:06 +0300

    Add data utils to __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,3 +4,4 @@ from src.utils.evaluation import *
 from src.utils.visualization import plot_loss, plot_losses
 from src.utils.log import configure_logger
 from src.utils.training import get_loaders
+from src.utils.data import get_datasets, get_dataset
